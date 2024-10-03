a12b3f0783093341a493591f9a7adec73bde230b
fan-droide <jose.gilpanal@telecom-paris.fr>
2024-10-03 11:20:07 +0200

    display error of access to mic

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -7,11 +7,13 @@ const TEST_LAT_MLS_BTN_ID = 'testlatencymlsbtn'
 const constraints = { audio: {echoCancellation:false, noiseSuppression:false, autoGainControl:false, latency: 0, channelCount: 1 }}
 
 const main = async () => {
-    
-    const stream = await navigator.mediaDevices.getUserMedia(constraints)
-
-    const ac = new AudioContext({latencyHint:0})
-
-    TestLatencyMLS.initialize(ac, stream, TEST_LAT_MLS_BTN_ID, debugCanvas)
+    try {
+        const stream = await navigator.mediaDevices.getUserMedia(constraints)
+        console.log('stream')
+        const ac = new AudioContext({latencyHint:0})
+        TestLatencyMLS.initialize(ac, stream, TEST_LAT_MLS_BTN_ID, debugCanvas)
+    } catch (error) {
+        document.getElementById('log-message').innerText = error
+    }
 }
 main()
