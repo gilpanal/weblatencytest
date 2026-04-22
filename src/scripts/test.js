6dbb3e47d44f2ecb1a1f1f2d5da533efa2390cb1
gilpanal <jose.gilpanal@telecom-paris.fr>
2026-04-22 11:52:36 +0200

    clean and remove not relevant code

--- src/scripts/test.js
+++ src/scripts/test.js
@@ -1,25 +1,10 @@
-import { drawResults, clearCanvas, drawLatencyHistogram } from './helper'
 import { generateMLS } from './mls'
 
 const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent)
-
-const CANVAS = `<div class='container' id='audio-area'>
-                    <canvas id='leftChannelCanvas' width='800' height='100' style='border:1px solid #000000;'></canvas>
-                    <canvas id='rightChannelCanvas' width='800' height='100' style='border:1px solid #000000;' hidden></canvas>
-                    <br>
-                    <canvas id='autocorrelationCanvas1' style='border:1px solid #000000;'></canvas>
-                    <canvas id='autocorrelationCanvas2' style='border:1px solid #000000;' hidden></canvas>
-                </div>`
-
-let COUNTER = 0
-
-let LATENCYTESTRESULTS = []
-
+                
 export class TestLatencyMLS {
 
-    noiseBuffer = null
-
-    debugCanvas = false
+    noiseBuffer = null   
     
     audioContext = null
 
@@ -31,9 +16,7 @@ export class TestLatencyMLS {
 
     inputStream = null
 
-    recordGainNode = null
-
-    numberOfTests = 1
+    recordGainNode = null   
 
     static getCorrectStreamForSafari(stream){
         const safariVersionIndex = navigator.userAgent.indexOf('Version/')
@@ -58,11 +41,9 @@ export class TestLatencyMLS {
         }
     }
 
-    static async initialize(ac, stream, btnId, debugCanvas, numTests) {
+    static async initialize(ac, stream, btnId, numTests) {
 
-        TestLatencyMLS.btnId = btnId
-
-        TestLatencyMLS.numberOfTests = numTests || 1
+        TestLatencyMLS.btnId = btnId        
 
         TestLatencyMLS.worker = new Worker(
             new URL('worker.js', import.meta.url),
@@ -71,12 +52,6 @@ export class TestLatencyMLS {
         TestLatencyMLS.worker.addEventListener('message', (message) => {
             TestLatencyMLS.workerMessageHanlder(message)
         })
-                 
-        if(debugCanvas){
-            console.log('AudioContext', ac)
-            TestLatencyMLS.debugCanvas = debugCanvas
-            document.getElementById('page-header').insertAdjacentHTML('afterend', CANVAS)
-        }        
             
         TestLatencyMLS.audioContext = ac
         TestLatencyMLS.onAudioPermissionGranted(stream)
@@ -89,12 +64,6 @@ export class TestLatencyMLS {
         const userMediaStream =  TestLatencyMLS.getCorrectStreamForSafari(inputStream)
         
         TestLatencyMLS.inputStream = userMediaStream
-        if(TestLatencyMLS.debugCanvas){
-            userMediaStream.getAudioTracks().forEach(async function(track) {
-                console.log('Test Latency Track Settings', track)
-                console.log('Test Latency Track Settings', track.getSettings())
-            })
-        }
         TestLatencyMLS.displayStart()
     }
 
@@ -108,9 +77,6 @@ export class TestLatencyMLS {
         TestLatencyMLS.startbutton.onclick = TestLatencyMLS.onAudioSetupFinished
         TestLatencyMLS.content.appendChild(TestLatencyMLS.startbutton)
     
-        if(TestLatencyMLS.debugCanvas){
-            clearCanvas()
-        }
     }
 
     static async onAudioSetupFinished() {
@@ -179,49 +145,15 @@ export class TestLatencyMLS {
             })
         }
         if(message.data.peakValuePow){                 
-            TestLatencyMLS.displayresults(message.data, TestLatencyMLS.signalrecorded, TestLatencyMLS.noiseBuffer, TestLatencyMLS.correlation)
-            if(message.data.channel === 0){
-                TestLatencyMLS.testLoop()
-            }            
+            TestLatencyMLS.displayresults(message.data, TestLatencyMLS.signalrecorded, TestLatencyMLS.noiseBuffer, TestLatencyMLS.correlation)                      
         }
-    }
-
-    static testLoop (){
-        COUNTER++
-        console.log('Looping test', COUNTER)
-        if (COUNTER < TestLatencyMLS.numberOfTests){
-            setTimeout(() => {
-                TestLatencyMLS.onAudioSetupFinished()
-            }, 1000)
-        } else{
-            COUNTER = 0
-            const meanLatency = LATENCYTESTRESULTS.reduce((sum, result) => sum + result.latency, 0) / LATENCYTESTRESULTS.length
-            const stdLatency = Math.sqrt(
-                LATENCYTESTRESULTS.reduce((sum, result) => sum + Math.pow(result.latency - meanLatency, 2), 0) / LATENCYTESTRESULTS.length
-            )
-            const meanRatio = LATENCYTESTRESULTS.reduce((sum, result) => sum + result.ratio, 0) / LATENCYTESTRESULTS.length
-            const stdRatio = Math.sqrt(
-                LATENCYTESTRESULTS.reduce((sum, result) => sum + Math.pow(result.ratio - meanRatio, 2), 0) / LATENCYTESTRESULTS.length
-            )
-            const latencies = LATENCYTESTRESULTS.map(result => result.latency)
-            const minLatency = Math.min(...latencies)
-            const maxLatency = Math.max(...latencies)
-            //console.log(LATENCYTESTRESULTS)
-            drawLatencyHistogram(LATENCYTESTRESULTS, 'latencyHistogram')          
-            document.getElementById('log-message').innerText = `Mean latency: ${meanLatency.toFixed(2)} ms, Std deviation latency: ${stdLatency.toFixed(2)}, Min latency: ${minLatency.toFixed(2)} ms, Max latency: ${maxLatency.toFixed(2)} ms, Mean ratio: ${meanRatio.toFixed(2)} dB, Std deviation ratio: ${stdRatio.toFixed(2)}`
-        }
-    }
+    }  
 
     static async displayAudioTagElem(chunks, mimeType) {
         
         const recordedAudio = new Blob(chunks, { type: mimeType })
         
-        TestLatencyMLS.signalrecorded = await TestLatencyMLS.blobToAudioBuffer(TestLatencyMLS.audioContext, recordedAudio)
-        
-        if(TestLatencyMLS.debugCanvas){
-            console.log('signalrecorded', TestLatencyMLS.signalrecorded)
-            console.log('mlssignal', TestLatencyMLS.noiseBuffer)
-        }
+        TestLatencyMLS.signalrecorded = await TestLatencyMLS.blobToAudioBuffer(TestLatencyMLS.audioContext, recordedAudio)       
         
         TestLatencyMLS.correlation = null
         TestLatencyMLS.worker.postMessage({
@@ -249,47 +181,19 @@ export class TestLatencyMLS {
        
         if(peak.channel === 0){
             const roundtriplatency = Number(peak.peakIndex / mlssignal.sampleRate * 1000).toFixed(2)
-            const ratioIs = 10 * Math.log10(peak.peakValuePow / peak.mean)        
-           
-            LATENCYTESTRESULTS.push({
-                latency: Number(roundtriplatency),
-                ratio: ratioIs,
-                timestamp: Date.now()
-            })
-            
-            console.log('Corr Ratio', ratioIs)
+            const ratioIs = 10 * Math.log10(peak.peakValuePow / peak.mean)
             if(ratioIs <= 18){
                 console.error('The Latency Test did not go well, there could be an issue with the audio settings')
             }
-            //console.log('Corr ABS(Ratio)', Math.abs(ratioIs))
             TestLatencyMLS.startbutton.innerText = 'TEST AGAIN '
             TestLatencyMLS.startbutton.innerHTML += `<span class='badge badge-info'>lat: ${roundtriplatency} ms.</span><br>`
             TestLatencyMLS.startbutton.innerHTML += `<span class='badge badge-light'>ratio: ${ratioIs.toFixed(2)} dB</span>`
-            if(TestLatencyMLS.debugCanvas) {
-                console.log('Channel', peak.channel )
-                console.log('Latency = ', roundtriplatency + ' ms')
-                drawResults(signalrecorded.getChannelData(0), 'leftChannelCanvas', 'autocorrelationCanvas1', correlation)
-                console.log('signalrecorded.numberOfChannels', signalrecorded.numberOfChannels)
-                if(signalrecorded.numberOfChannels>1){
-                    document.getElementById('rightChannelCanvas').hidden = false
-                    document.getElementById('autocorrelationCanvas2').hidden = false
-                    TestLatencyMLS.correlation = null
-                    TestLatencyMLS.worker.postMessage({
-                        command: 'correlation',
-                        data1: TestLatencyMLS.signalrecorded.getChannelData(1), 
-                        data2: TestLatencyMLS.noiseBuffer.getChannelData(0), 
-                        maxLag: (0.600 * TestLatencyMLS.audioContext.sampleRate),
-                        channel: 1
-                    })
-                }
-            }
         } else{
             console.log('Channel', peak.channel )
             const roundtriplatency = peak.peakIndex / mlssignal.sampleRate * 1000
             console.log('Latency = ', roundtriplatency + ' ms')
             const ratioIs = 10 * Math.log10(peak.peakValuePow / peak.mean)
             console.log('Corr Ratio', ratioIs)
-            drawResults(signalrecorded.getChannelData(1),  'rightChannelCanvas', 'autocorrelationCanvas2', TestLatencyMLS.correlation)
         }      
     }
 }
